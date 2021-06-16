2a17bdec6874ad3d22b7a8d5bfaf76f2841b559a
Fatih <68826839+mb-later@users.noreply.github.com>
2021-06-16 16:50:58 +0300

    Update __resource.lua

--- mbl-multichar/mbl-multicharacterv2/__resource.lua
+++ mbl-multichar/mbl-multicharacterv2/__resource.lua
@@ -1,4 +1,4 @@
---[[ DUMPED USING FIVEX ]]--
+
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 ui_page "html/index.html"
@@ -17,4 +17,3 @@ files {
     'html/reset.css',
     'html/script.js',
 }
---[[ DUMPED USING FIVEX ]]--
